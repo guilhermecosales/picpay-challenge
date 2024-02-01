274e2ed955389588c7d5133eb42ed9b6ff6ecde9
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-02-01 09:56:54 -0300

    feat: validate if is merchant user

--- src/main/java/com/picpay/picpaychallenge/enumerated/UserType.java
+++ src/main/java/com/picpay/picpaychallenge/enumerated/UserType.java
@@ -3,6 +3,10 @@ package com.picpay.picpaychallenge.enumerated;
 public enum UserType {
 
     COMMON,
-    MERCHANT
+    MERCHANT;
+
+    public boolean isMerchant() {
+        return this == MERCHANT;
+    }
 
 }
