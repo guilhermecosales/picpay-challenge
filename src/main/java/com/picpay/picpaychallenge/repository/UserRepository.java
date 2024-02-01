347c9be931f2d3ba4a2be171f1d4fc986d128204
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-02-01 00:22:52 -0300

    feat: find by document

--- src/main/java/com/picpay/picpaychallenge/repository/UserRepository.java
+++ src/main/java/com/picpay/picpaychallenge/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.picpay.picpaychallenge.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByDocument(String document);
+
 }
