1937644e261ee2592089751742a1181843b396ed
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-02-02 14:28:57 -0300

    feat: validation constraints

--- src/main/java/com/picpay/picpaychallenge/dto/request/TransactionRequestDto.java
+++ src/main/java/com/picpay/picpaychallenge/dto/request/TransactionRequestDto.java
@@ -1,13 +1,23 @@
 package com.picpay.picpaychallenge.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 import java.math.BigDecimal;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record TransactionRequestDto(
+        @NotNull
+        @PositiveOrZero
         BigDecimal amount,
+
+        @NotNull
+        @Positive
         Long payerId,
+
+        @Positive
         Long payeeId
 ) {
 }
--- src/main/java/com/picpay/picpaychallenge/dto/request/UserRequestDto.java
+++ src/main/java/com/picpay/picpaychallenge/dto/request/UserRequestDto.java
@@ -2,18 +2,35 @@ package com.picpay.picpaychallenge.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.picpay.picpaychallenge.enumerated.UserType;
+import jakarta.validation.constraints.*;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record UserRequestDto(
+        @NotEmpty
+        @Size(min = 3, max = 20)
         String firstName,
+
+        @NotEmpty
+        @Size(min = 3, max = 50)
         String lastName,
+
         String document,
+
+        @Email
+        @NotEmpty
+        @Size(max = 60)
         String email,
+
+        @NotEmpty
         String password,
+
+        @NotNull
         UserType userType,
+
+        @PositiveOrZero
         BigDecimal balance
 ) implements Serializable {
 }
