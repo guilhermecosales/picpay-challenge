c4c85699ad98dc11edc58380e937e7b3d2a27279
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-02-02 15:58:42 -0300

    feat: send users data to authorizer

--- src/main/java/com/picpay/picpaychallenge/service/client/AuthorizerService.java
+++ src/main/java/com/picpay/picpaychallenge/service/client/AuthorizerService.java
@@ -3,8 +3,13 @@ package com.picpay.picpaychallenge.service.client;
 import com.picpay.picpaychallenge.client.AuthorizerClient;
 import com.picpay.picpaychallenge.client.response.AuthorizerResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.math.BigDecimal;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthorizerService {
@@ -13,8 +18,14 @@ public class AuthorizerService {
 
     private final AuthorizerClient authorizerClient;
 
-    public boolean isAuthorizedTransaction() {
+    public boolean isAuthorizedTransaction(Long payerId, Long payeeId, BigDecimal amount) {
         AuthorizerResponse clientResponse = authorizerClient.authorize();
+
+        if (StringUtils.hasText(SUCCESS_MESSAGE)) {
+            log.info("Authorized transaction.");
+            log.info("Payer: {}, Payee: {} and Amount: {}", payerId, payeeId, amount);
+        }
+
         return clientResponse.message().contentEquals(SUCCESS_MESSAGE);
     }
 
