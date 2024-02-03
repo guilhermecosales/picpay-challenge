0ffd1848b9b43bc61966e7072a099ebc48f032ce
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-02-02 23:31:53 -0300

    fix: add instance of the interface implementation

--- src/main/java/com/picpay/picpaychallenge/mapper/TransactionMapper.java
+++ src/main/java/com/picpay/picpaychallenge/mapper/TransactionMapper.java
@@ -4,9 +4,12 @@ import com.picpay.picpaychallenge.dto.common.TransactionDto;
 import com.picpay.picpaychallenge.dto.request.TransactionRequestDto;
 import com.picpay.picpaychallenge.entity.Transaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface TransactionMapper {
+
+    TransactionMapper INSTANCE = Mappers.getMapper(TransactionMapper.class);
 
     Transaction transactionRequestDtoToTransaction(TransactionRequestDto from);
 
--- src/main/java/com/picpay/picpaychallenge/mapper/UserMapper.java
+++ src/main/java/com/picpay/picpaychallenge/mapper/UserMapper.java
@@ -4,9 +4,12 @@ import com.picpay.picpaychallenge.dto.common.UserDto;
 import com.picpay.picpaychallenge.dto.request.UserRequestDto;
 import com.picpay.picpaychallenge.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     UserDto userToUserDto(User from);
 
