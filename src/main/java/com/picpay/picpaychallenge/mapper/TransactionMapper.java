d671c00532b624c97523013bc8805711c1f94107
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-02-04 15:34:49 -0300

    feat: remove instance and add field mapping

--- src/main/java/com/picpay/picpaychallenge/mapper/TransactionMapper.java
+++ src/main/java/com/picpay/picpaychallenge/mapper/TransactionMapper.java
@@ -4,15 +4,17 @@ import com.picpay.picpaychallenge.dto.common.TransactionDto;
 import com.picpay.picpaychallenge.dto.request.TransactionRequestDto;
 import com.picpay.picpaychallenge.entity.Transaction;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TransactionMapper {
 
-    TransactionMapper INSTANCE = Mappers.getMapper(TransactionMapper.class);
-
+    @Mapping(source = "payerId", target = "payer.id")
+    @Mapping(source = "payeeId", target = "payee.id")
     Transaction transactionRequestDtoToTransaction(TransactionRequestDto from);
 
+    @Mapping(source = "payer.id", target = "payerId")
+    @Mapping(source = "payee.id", target = "payeeId")
     TransactionDto transactionToTransactionDto(Transaction from);
 
 }
