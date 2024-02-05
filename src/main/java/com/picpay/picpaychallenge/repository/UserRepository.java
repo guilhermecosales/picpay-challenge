b21a2847fe0225c427e989e666a6b065bb79d5cd
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-02-04 23:57:43 -0300

    refactor: remove unused method

--- src/main/java/com/picpay/picpaychallenge/repository/UserRepository.java
+++ src/main/java/com/picpay/picpaychallenge/repository/UserRepository.java
@@ -4,12 +4,8 @@ import com.picpay.picpaychallenge.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByDocument(String document);
 
     boolean existsByDocument(String document);
 
