61fac52ece6f915563ab8ef12b1a17efa85a1330
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-02-07 23:40:40 -0300

    feat: map document fields

--- src/main/java/com/picpay/picpaychallenge/mapper/UserMapper.java
+++ src/main/java/com/picpay/picpaychallenge/mapper/UserMapper.java
@@ -4,12 +4,17 @@ import com.picpay.picpaychallenge.dto.common.UserDto;
 import com.picpay.picpaychallenge.dto.request.UserRequestDto;
 import com.picpay.picpaychallenge.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "document.cpf", source = "document.cpf")
+    @Mapping(target = "document.cnpj", source = "document.cnpj")
     UserDto userToUserDto(User from);
 
+    @Mapping(target = "document.cpf", source = "document.cpf")
+    @Mapping(target = "document.cnpj", source = "document.cnpj")
     User userRequestDtoToUser(UserRequestDto from);
 
 }
