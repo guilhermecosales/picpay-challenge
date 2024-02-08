67677b638a0d22a0e72eee26cf94da68fa0b7418
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-02-07 23:38:00 -0300

    feat: override handle method argument not valid

--- src/main/java/com/picpay/picpaychallenge/exception/handler/RestExceptionHandler.java
+++ src/main/java/com/picpay/picpaychallenge/exception/handler/RestExceptionHandler.java
@@ -1,13 +1,16 @@
 package com.picpay.picpaychallenge.exception.handler;
 
 import com.picpay.picpaychallenge.exception.custom.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -23,6 +26,19 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleCustomException(RuntimeException ex) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, ex.getMessage());
+        return ResponseEntity.status(status).body(problemDetail);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, "Invalid request content.");
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError ->
+                errors.put(fieldError.getField(), fieldError.getDefaultMessage()));
+
+        problemDetail.setProperty("errors", errors);
+
         return ResponseEntity.status(status).body(problemDetail);
     }
 
